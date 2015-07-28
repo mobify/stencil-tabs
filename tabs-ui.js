167a3cfbbcde08964fc6cbea3ffc823182268a04
Ryan Frederick <ryan@ryanfrederick.com>
2015-07-28 16:14:15 -0700

    First, simple implementation.

--- tabs-ui.js
+++ tabs-ui.js
@@ -1,11 +1,95 @@
-define([
-    '$'
-], function($) {
+define(['$'], function($) {
+    /**
+     * Coordinator singleton
+     *
+     * Manages setting unique ids for tabs/tabpanes.
+     */
+    var TabsCoordinator = function() {
+        if (TabsCoordinator.prototype._singletonInstance) {
+            return TabsCoordinator.prototype._singletonInstance;
+        }
 
-    var init = function() {
+        this.counter = 0;
+
+        TabsCoordinator.prototype._singletonInstance = this;
+    }
+
+    /**
+     * Get the next available id
+     */
+    TabsCoordinator.prototype.nextIndex = function() {
+        return this.counter++;
+    };
+
+    /**
+     * Tabs constructor
+     */
+    var Tabs = function($el, options) {
+        var self = this;
+        var coordinator = new TabsCoordinator();
+
+        this.instanceId = coordinator.nextIndex();
+        this.$el = $el;
+        this.$tabs = $el.find('.c-tabs__tab');
+        this.$panes = $el.find('.c-tabs__pane');
+        this.selectedIndex = parseInt(this.$el.attr('data-selected-index'), 10) || 0;
+
+        this.$tabs.each(function(i, tab) {
+            var tabId = 'stencil-tab-' + self.instanceId + '.' + i;
+            var paneId = 'stencil-tabpane-' + self.instanceId + '.' + i;
+
+            $(tab).attr({
+                'id': tabId,
+                'aria-controls': paneId,
+                'tabindex': '0',
+                'aria-selected': 'false',
+            });
+
+            self.$panes.eq(i).attr({
+                'id': paneId,
+                'aria-labelledby': tabId,
+                'aria-hidden': 'true',
+            });
+        });
+
+        this.$el.on('click', '.c-tabs__tab', function(event) {
+            self.select(self.$tabs.index(this));
+        });
+
+        this.select(this.selectedIndex);
+    }
+
+    Tabs.prototype.select = function(index) {
+        if (this.selectedIndex === index && this.$tabs.find('.c--is-selected').length) {
+            return;
+        }
+
+        this.$tabs.eq(this.selectedIndex).attr({
+            'tabindex': '-1',
+            'aria-selected': 'false',
+        }).removeClass('c--is-selected');
+
+        this.$panes.eq(this.selectedIndex).attr({
+            'aria-hidden': 'true',
+        }).removeClass('c--is-selected');
+
+        this.$tabs.eq(index).attr({
+            'tabindex': '0',
+            'aria-selected': 'true',
+        }).addClass('c--is-selected');
+
+        this.$panes.eq(index)
+            .removeAttr('aria-hidden')
+            .addClass('c--is-selected');
+
+        this.selectedIndex = index;
     };
 
     return {
-        init: init
+        init: function($el, options) {
+            // If already initialized, return the instance; otherwise, create it
+            // and expose it through `$('.c-tabs').data('component')`.
+            return $el.data('component') || $el.data('component', new Tabs($el, options));
+        }
     };
 });
